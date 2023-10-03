82a936ce2b2abdfae9345cdf2ceb9305aa3500d0
keith <keith.wachira@students.moringaschol.com>
2023-10-03 15:17:29 +0300

    task two commpleted

--- index.js
+++ index.js
@@ -1,1 +1,68 @@
 // Write your code in this file!
+const current User ='keith wachira';
+const welcome Message ='Welocme to Flatbook, ';
+const currentUser ='Keith Wachira';
+const welcome Message = currentUser;
+const currentUser = 'Keith Wachira';
+
+const welcomeMessage = 'Welcome to Flatbook, currentUser';
+AssertionError: expected 'Welcome to Flatbook, currentUser' to contain 'Keith Wachira'
+typeof "const currentUser = 'Grace Hopper';";
+//=> "string"
+
+"const currentUser = 'Keith Wachira';".length;
+//=> 35
+
+currentUser;
+//=> Uncaught ReferenceError: currentUser is not defined
+"High " + "five!";
+//=> "High five!"
+
+"We" + ' ' + `can` + " " + 'concat' + `enate` + " as many strings " + 'as our heart ' + `desires.`;
+//=> "We can concatenate as many strings as our heart desires." 
+const currentUser = 'keith Wachira';
+
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
+const myString = 'template literal';
+
+const myNumber = 10;
+
+const myBoolean = false;
+
+`Saying that interpolation with ${myString}s is better than concatenation ${90 + myNumber}% of the time is simply ${myBoolean}. But it is pretty cool!
+
+Beware that new lines inside of a ${myString} will be preserved as new lines in the resulting ${typeof myString}!`;
+//=> "Saying that interpolation with template literals is better than concatenation 100% of the time is simply false. But it is pretty cool!
+
+// Beware that new lines inside of a template literal will be preserved as new lines in the resulting string!"
+const currentUser = 'keith wachira';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}`;
+const currentUser = 'keith wachira';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+Edsger Dijkstra'[0]' ;
+//=> "E"
+
+'Edsger Dijkstra'.charAt(0);
+//=> "E"
+'Edsger Dijkstra'.length;
+//=> 15
+
+'Edsger Dijkstra'[15];
+//=> undefined
+
+'Edsger Dijkstra'[14];
+//=> "a"
+'Edsger Dijkstra'.slice();
+//=> "Edsger Dijkstra"
+'Edsger Dijkstra'.slice(7);
+//=> "Dijkstra"
+'Edsger Dijkstra'.slice(0, 3);
+//=> "Eds"
+currentUser.slice(0, 1); 
+const currentUser = 'keith wachira';
+
+...
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
